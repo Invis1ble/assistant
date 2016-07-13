9e7c01005bf022790c661cb22692b0d226888fce
Invis1ble <invisiblexman2010@gmail.com>
2016-07-13 20:14:49 +0300

    Implemented period patching

--- src/AppBundle/Controller/PeriodController.php
+++ src/AppBundle/Controller/PeriodController.php
@@ -2,11 +2,16 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Form;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\View\View;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 use AppBundle\Entity\Period;
+use AppBundle\Form\Type\TaskPeriodFormType;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * PeriodController
@@ -43,5 +48,44 @@ class PeriodController extends FOSRestController
     public function getPeriodAction(Period $period): Period
     {
         return $period;
+    }
+
+    /**
+     * Patch existing period from the submitted data.
+     *
+     * @ApiDoc(
+     *     statusCodes = {
+     *         204 = "Returned when successful",
+     *         400 = "Returned when the form has errors",
+     *         404 = "Returned when the period is not found"
+     *     },
+     *     requirements = {
+     *         {
+     *             "name" = "period",
+     *             "dataType" = "UUID string",
+     *             "description" = "Period ID"
+     *         }
+     *     }
+     * )
+     *
+     * @param Request $request
+     * @param Period  $period
+     *
+     * @return View|Form
+     */
+    public function patchPeriodAction(Request $request, Period $period)
+    {
+        $form = $this->createForm(TaskPeriodFormType::class, $period);
+        $form->submit(json_decode($request->getContent(), true), false);
+
+        if ($form->isValid()) {
+            $this->get('app.manager.period_manager')->save($period);
+
+            return $this->routeRedirectView('api_get_period', [
+                'period' => $period->getId(),
+            ], Response::HTTP_NO_CONTENT);
+        }
+
+        return $form;
     }
 }
