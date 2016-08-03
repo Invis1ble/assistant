a3bcfea18e4b850da071a7a9dbc68c471b33cb0f
Invis1ble <invisiblexman2010@gmail.com>
2016-08-03 18:21:38 +0300

    Fixed test

--- tests/AppBundle/DataFixtures/Faker/Provider/PeriodProviderTest.php
+++ tests/AppBundle/DataFixtures/Faker/Provider/PeriodProviderTest.php
@@ -25,7 +25,6 @@ class PeriodProviderTest extends KernelTestCase
     public function startedAtProvider(): array
     {
         $nowTimestamp = time();
-        $nowDateTime = new DateTime('@' . $nowTimestamp);
 
         $task1 = new Task();
         $task1CreatedAt = new DateTime('@' . ($nowTimestamp - 10));
@@ -49,23 +48,22 @@ class PeriodProviderTest extends KernelTestCase
         $task2Period->setTaskAndAddSelfToPeriodsCollection($task2);
 
         return [
-            [$task1Period, $task1CreatedAt, $nowDateTime],
-            [$task2Period, $task2Period1FinishedAt, $nowDateTime],
+            [$task1Period, $task1CreatedAt],
+            [$task2Period, $task2Period1FinishedAt],
         ];
     }
 
     /**
      * @param Period   $period
      * @param DateTime $startDateTime
-     * @param DateTime $endDateTime
      *
      * @dataProvider startedAtProvider
      */
-    public function testStartedAt(Period $period, DateTime $startDateTime, DateTime $endDateTime)
+    public function testStartedAt(Period $period, DateTime $startDateTime)
     {
         $startedAt = PeriodProvider::startedAt($period);
 
         $this->assertGreaterThanOrEqual($startDateTime, $startedAt);
-        $this->assertLessThanOrEqual($endDateTime, $startedAt);
+        $this->assertLessThanOrEqual(new DateTime(), $startedAt);
     }
 }
